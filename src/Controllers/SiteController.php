048ba05f2197a700e5f9a2b550a10289dfda7dfe
viniciusam <viniciusam.22@gmail.com>
2024-03-06 10:44:17 -0300

    Organização do código nos helpers

--- src/Controllers/SiteController.php
+++ src/Controllers/SiteController.php
@@ -12,11 +12,10 @@ class SiteController extends Controller
     {
         $video = new Video();
         $videos = $video->all();
-        $videos_views = new VideosView();
 
         foreach ($videos as $video) {
-            $views = $this->getViews($videos_views, $video);
-            $daysTo = $this->daysTo($views, $video);
+            $views = VideosHelper::views($video);
+            $daysTo = VideosHelper::daysTo($video, $views);
 
             $video->views = $views;
             $video->formatted_views = number_format($views, 0, ',', '.');
@@ -41,51 +40,5 @@ class SiteController extends Controller
     public function notFound(): void
     {
         $this->view('notFound.php');
-    }
-
-    /**
-     * @param VideosView $videos_views
-     * @param mixed $video
-     * @return int
-     */
-    public function getViews(VideosView $videos_views, mixed $video): int
-    {
-        $view = $videos_views->last("WHERE video_id = $video->id");
-        $crated_at = date('Y-m-d h:i:s', strtotime($view->created_at));
-        $ten_minutes_ago = date('Y-m-d h:i:s', strtotime('-10 minutes'));
-
-        $views = $view->views;
-
-        if (!$view || $crated_at < $ten_minutes_ago) {
-            $views = VideosHelper::views($video->key);
-            $videos_views->create([
-                'video_id' => $video->id,
-                'views' => $views,
-                'created_at' => date('Y-m-d H:i:s'),
-            ]);
-        }
-
-        return $views;
-    }
-
-    /**
-     * @param int $views
-     * @param mixed $video
-     * @return array
-     */
-    public function daysTo(int $views, mixed $video): array
-    {
-        $published_at = date('Y-m-d', strtotime($video->published_at));
-        $today = date('Y-m-d');
-        $days = (strtotime($today) - strtotime($published_at)) / (60 * 60 * 24);
-
-        $next = ceil($views / 100000000) * 100000000;
-
-        $media = $views / $days;
-
-        return [
-            'days' => round(($next - $views) / $media),
-            'next' => $next
-        ];
     }
 }
--- src/Helpers/VideosHelper.php
+++ src/Helpers/VideosHelper.php
@@ -3,22 +3,74 @@
 namespace Helpers;
 
 use Dotenv\Dotenv;
+use Models\VideosView;
 
 class VideosHelper
 {
-    public static function views(string $key): int
+    public static function views($video): int
     {
         $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
         $dotenv->load();
 
+        $videos_views = new VideosView();
+
+        $view = $videos_views->last("WHERE video_id = $video->id");
+        $created_at = date('Y-m-d H:i:s', strtotime($view->created_at));
+        $ten_minutes_ago = date('Y-m-d H:i:s', strtotime('-10 minutes'));
+
+        $views = $view->views;
+
+        if (!$view || $created_at < $ten_minutes_ago) {
+            $views = self::fetchViews($video, $videos_views);
+        }
+
+        return $views;
+    }
+
+    /**
+     * @param mixed $video
+     * @param int $views
+     * @return array
+     */
+    public static function daysTo(mixed $video, int $views): array
+    {
+        $published_at = date('Y-m-d', strtotime($video->published_at));
+        $today = date('Y-m-d');
+        $days = (strtotime($today) - strtotime($published_at)) / (60 * 60 * 24);
+
+        $next = ceil($views / 100000000) * 100000000;
+
+        $media = $views / $days;
+
+        return [
+            'days' => round(($next - $views) / $media),
+            'next' => $next
+        ];
+    }
+
+    /**
+     * @param $video
+     * @param VideosView $videos_views
+     * @return mixed
+     */
+    private static function fetchViews($video, VideosView $videos_views): mixed
+    {
         $url1 = 'https://www.googleapis.com/youtube/v3/videos?id=';
         $url2 = '&key=' . $_ENV['YOUTUBE_API_KEY'] . '&fields=items(snippet(title,publishedAt),statistics(viewCount,likeCount))&part=snippet,statistics';
 
-        $url = $url1 . $key . $url2;
+        $url = $url1 . $video->key . $url2;
 
         $json = file_get_contents($url);
         $data = json_decode($json);
 
-        return $data->items[0]->statistics->viewCount;
+        $views = $data->items[0]->statistics->viewCount;
+
+        $videos_views->create([
+            'video_id' => $video->id,
+            'views' => $views,
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        return $views;
     }
 }
